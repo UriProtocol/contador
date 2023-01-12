dde6259b1d97b163884173990fd6fffe9c51ffc0
UriProtocol <angel_3041210085@utd.edu.mx>
2023-01-12 11:58:42 -0600

    v4

--- src/components/contador.js
+++ src/components/contador.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
-const Contador = () =>(
-    <>
-        <p> Contador :0</p>
-        <Button variant="dark">+</Button>
-    </>
-)
+const Contador = () =>{
+
+    const [contador, setContador] = useState(0)
+    
+    const sumar = () =>{
+        let contador1  = contador
+        contador1++;
+
+        setContador(contador1)
+    }
+    const restar = () =>{
+        let contador1  = contador
+        contador1--;
+
+        setContador(contador1)
+    }
+    const reiniciar = () =>{
+        setContador(0)
+    }
+
+    return (
+        <>
+            <p> Contador : {contador}</p>
+            <Button variant="dark" className='bs-button' onClick={sumar}>+</Button>
+            <Button variant="secondary" className='bs-button' onClick={restar}>-</Button>
+            <Button variant="info" className='bs-button' onClick={reiniciar}>Reiniciar</Button>
+        </>
+    )
+}
+
 
 export default Contador
