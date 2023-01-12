95a7a1aa776bdf2ec885cbdc9ea40e2c00d9aa94
UriProtocol <angel_3041210085@utd.edu.mx>
2023-01-12 11:38:57 -0600

    v3

---
+++ src/components/contador.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+const Contador = () =>(
+    <>
+        <p> Contador :0</p>
+        <Button variant="dark">+</Button>
+    </>
+)
+
+export default Contador
